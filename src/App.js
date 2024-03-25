b5dbc1ee0cc5faf095d90a76ef989f1d160fdefe
Rahul <bhaleraor17@gmail.com>
2024-03-25 15:18:31 +0530

    x-countries-complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useEffect } from "react";
+import { useState } from "react";
 import './App.css';
-
+import axios from "axios";
 function App() {
+  const [countries, setCountries] = useState([]);
+  let fetchCountry = async () => {
+    try {
+      let response = await axios.get("https://restcountries.com/v3.1/all");
+      setCountries(response.data);
+    }
+    catch (e) {
+      console.log(e);
+    }
+
+  }
+  const cardStyle = {
+    width: "150px",
+    height: "150px",
+    display: "flex",
+    flexDirection: "column",
+    border: "1px solid black",
+    borderRadius: "10px",
+    padding: "10px",
+    margin: "10px",
+    textAlign: "center"
+
+
+  }
+  const imgStyle = {
+    width: "100%",
+    height: "80%"
+  }
+  const container = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center"
+
+  }
+  useEffect(() => {
+    fetchCountry();
+    console.log(countries);
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={container}>
+      {countries.map((country) => (<div style={cardStyle}><img style={imgStyle} src={country.flags.png} alt={country.name.common} />
+        <h3>{country.name.common}</h3></div>))}
+
     </div>
   );
 }
